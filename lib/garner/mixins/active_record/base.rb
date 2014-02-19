0220b334834fd37cbeaee4e7fd25286162a91305
Matt Beedle <mattbeedle@googlemail.com>
2014-02-19 19:51:56 +0100

    Make ActiveRecord mixin work

--- lib/garner/mixins/active_record/base.rb
+++ lib/garner/mixins/active_record/base.rb
@@ -10,6 +10,20 @@ module Garner
 
         included do
           extend Garner::Cache::Binding
+
+          after_create  :_garner_after_create
+          after_update  :_garner_after_update
+          after_destroy :_garner_after_destroy
+        end
+
+        module ClassMethods
+          def proxy_binding
+            _latest_by_updated_at
+          end
+
+          def _latest_by_updated_at
+            order('updated_at DESC').first
+          end
         end
       end
     end
--- lib/garner/strategies/binding/key/safe_cache_key.rb
+++ lib/garner/strategies/binding/key/safe_cache_key.rb
@@ -3,7 +3,7 @@ module Garner
     module Binding
       module Key
         class SafeCacheKey < Base
-          VALID_FORMAT = /^(?<model>[^\/]+)\/(?<id>.+)-(?<timestamp>[0-9]{14})$/
+          #VALID_FORMAT = /^(?<model>[^\/]+)\/(?<id>.+)-(?<timestamp>[0-9]{14})$/
 
           # Compute a cache key from an object binding. Only return a key if
           # :cache_key and :updated_at are both defined and present on the
@@ -21,7 +21,7 @@ module Garner
             return unless binding.respond_to?(:updated_at) && binding.updated_at
 
             # Check for ActiveModel cache key format
-            return unless binding.cache_key =~ VALID_FORMAT
+            #return unless binding.cache_key =~ VALID_FORMAT
 
             decimal_portion = binding.updated_at.utc.to_f % 1
             decimal_string = sprintf("%.10f", decimal_portion).gsub(/^0/, "")
